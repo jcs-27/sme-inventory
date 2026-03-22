667c71a3c5328dfdf4763c05f6a8455387bdbfe0
Jim-Carey Toh <jimcareytoh@gmail.com>
2026-03-22 10:44:24 -0700

    feat: add edit and delete product views, templates, and buttons

--- inventory/views.py
+++ inventory/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
 from .forms import ProductForm
 
 def home(request):
     products = Product.objects.all()
-    form = ProductForm()
-    return render(request, 'inventory/product_list.html', {'products': products, 'form': form})
+    return render(request, 'inventory/product_list.html', {'products': products})
 
 def add_product(request):
     if request.method == 'POST':
@@ -16,3 +15,21 @@ def add_product(request):
     else:
         form = ProductForm()
     return render(request, 'inventory/add_product.html', {'form': form})
+
+def edit_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'inventory/edit_product.html', {'form': form, 'product': product})
+
+def delete_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('home')
+    return render(request, 'inventory/delete_product.html', {'product': product})
--- sme_core/urls.py
+++ sme_core/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('add/', views.add_product, name='add_product'),
+    path('<int:pk>/edit/', views.edit_product, name='edit_product'),
+    path('<int:pk>/delete/', views.delete_product, name='delete_product'),
 ]
