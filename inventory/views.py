861fdbb1b56fbc64fa815f72268fdbdf9784a2ab
Jim-Carey Toh <jimcareytoh@gmail.com>
2026-03-22 10:18:10 -0700

    feat: add product form and add_product view

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from .forms import ProductForm
 
 def home(request):
     products = Product.objects.all()
-    return render(request, 'inventory/product_list.html', {'products': products})
+    form = ProductForm()
+    return render(request, 'inventory/product_list.html', {'products': products, 'form': form})
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ProductForm()
+    return render(request, 'inventory/add_product.html', {'form': form})
